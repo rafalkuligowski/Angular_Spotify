ee2eb0fa2b2521a08b74317933a14e6d75d94049
Rafal <rafa@rafal-ThinkPad-T520>
2018-07-10 12:11:07 +0200

    Zmiana nazw komponentów

--- src/app/music/music.module.ts
+++ src/app/music/music.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MusicSearchComponent } from './music-search/music-search.component';
-import { MusicFormComponent } from './music-form/music-form.component';
-import { MusicGridComponent } from './music-grid/music-grid.component';
-import { MusicItemComponent } from './music-item/music-item.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { AlbumGridComponent } from './album-grid/album-grid.component';
 import { AlbumItemComponent } from './album-item/album-item.component';
@@ -12,7 +9,7 @@ import { AlbumItemComponent } from './album-item/album-item.component';
   imports: [
     CommonModule
   ],
-  declarations: [MusicSearchComponent, MusicFormComponent, MusicGridComponent, MusicItemComponent, SearchFormComponent, AlbumGridComponent, AlbumItemComponent],
+  declarations: [MusicSearchComponent, SearchFormComponent, AlbumGridComponent, AlbumItemComponent],
   exports: [MusicSearchComponent]
 })
 export class MusicModule { }
