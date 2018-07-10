f96335d57e038649e62b4fb3640e01e72ff3c784
Rafal <rafa@rafal-ThinkPad-T520>
2018-07-10 08:37:22 +0200

    ngFor

--- src/app/playlists/item-list/item-list.component.ts
+++ src/app/playlists/item-list/item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Playlist } from '../../model/playlist';
 
 @Component({
   selector: 'app-item-list',
@@ -7,6 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemListComponent implements OnInit {
 
+	playlists: Playlist[] = [
+		{
+			id: 123,
+			name: 'New hit 123',
+			color: '#ff0000',
+			favourite: true,
+		},
+		{
+			id: 234,
+			name: 'New hit 234',
+			color: '#00ff00',
+			favourite: false,
+		},
+		{
+			id: 456,
+			name: 'New hit 567',
+			color: '#0000ff',
+			favourite: false,
+		}
+	];
   constructor() { }
 
   ngOnInit() {
