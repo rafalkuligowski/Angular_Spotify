5cff7aee0cea0cf7ab86f650d3e0caca7834d577
Rafal <rafa@rafal-ThinkPad-T520>
2018-07-11 09:12:32 +0200

    Search form Reactive

--- src/app/music/music.module.ts
+++ src/app/music/music.module.ts
@@ -5,11 +5,13 @@ import { SearchFormComponent } from './search-form/search-form.component';
 import { AlbumGridComponent } from './album-grid/album-grid.component';
 import { AlbumItemComponent } from './album-item/album-item.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
 	CommonModule,
-	HttpClientModule
+	HttpClientModule,
+	ReactiveFormsModule
   ],
   declarations: [MusicSearchComponent, SearchFormComponent, AlbumGridComponent, AlbumItemComponent],
   exports: [MusicSearchComponent],
--- src/app/music/search-form/search-form.component.ts
+++ src/app/music/search-form/search-form.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-search-form',
-  templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.css']
+	selector: 'app-search-form',
+	templateUrl: './search-form.component.html',
+	styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
 
+	queryForm: FormGroup;
+
+	constructor() {
+		this.queryForm = new FormGroup({
+			query: new FormControl("Batman")
+		});
+		console.log(this.queryForm);
+
+
+		this.queryForm
+			.get("query")
+			.valueChanges.subscribe(value => this.search(value));
+	}
 	search(query) {
 		console.log(query);
 	}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
